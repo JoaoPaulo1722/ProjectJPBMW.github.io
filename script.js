eed496e7a1abc8ae1a185ad37429594ea89d9ca4
João Paulo Peres <peresjoao244@gmail.com>
2023-07-17 23:03:08 -0300

    Movendo o código do script.js para o index.html

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var redBtn = document.getElementById("red");
-var blueBtn = document.getElementById("blue");
-var blackBtn = document.getElementById("black");
-var bike = document.getElementById("bike");
+// var redBtn = document.getElementById("red");
+// var blueBtn = document.getElementById("blue");
+// var blackBtn = document.getElementById("black");
+// var bike = document.getElementById("bike");
 
-redBtn.onclick = function () {
-  bike.style.backgroundImage = "url(./images/BMW1.png)";
-};
+// redBtn.onclick = function () {
+//   bike.style.backgroundImage = "url(./images/BMW1.png)";
+// };
 
-blueBtn.onclick = function () {
-  bike.style.backgroundImage = "url(./images/BMW2.png)";
-};
-blackBtn.onclick = function () {
-  bike.style.backgroundImage = "url(./images/BMW3.png)";
-};
+// blueBtn.onclick = function () {
+//   bike.style.backgroundImage = "url(./images/BMW2.png)";
+// };
+// blackBtn.onclick = function () {
+//   bike.style.backgroundImage = "url(./images/BMW3.png)";
+// };
